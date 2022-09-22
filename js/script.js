24d26071d56b5f83bdc718fcc531884147325cef
Jonathan Ronning <jonathanronning@outlook.com>
2022-09-22 13:34:52 -0400

    getComputerChoice is successfully implemented with global variable as a response

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
-var choice; 
+let choice;
 
 function getComputerChoice(choice) {
     let computerChoice = Math.random()
     console.log(computerChoice)
     if (computerChoice >= 0.000 && computerChoice < 0.333) {
-        choice = 'rock'
-        return console.log(choice)
+        return choice = 'rock'
+        //return console.log(choice)
     } else if (computerChoice >= 0.333 && computerChoice < 0.666) {
-        choice = 'paper'                                                    //choice is not globally defined, should be an issue
-        return console.log(choice)
+        return choice = 'paper'                                                 
+        //return console.log(choice)
     } else if (computerChoice >= 0.666 && computerChoice < 1) {
-        choice = 'scissors'
-        return console.log(choice)
+        return choice = 'scissors'
+        //return console.log(choice)
     } else {
         return console.log('error in getComputerChoice')
     }
 }
-getComputerChoice(choice);
+let computerChoice = getComputerChoice(choice);
+console.log(`computer choice: is ${computerChoice}`)
