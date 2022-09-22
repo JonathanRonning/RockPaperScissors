d250bada67945a06fa55247f66fcb154e1092475
Jonathan Ronning <jonathanronning@outlook.com>
2022-09-22 13:21:30 -0400

    add getComputerChoice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+let choice;
+
+function getComputerChoice(choice) {
+    let computerChoice = Math.random()
+    console.log(computerChoice)
+    if (computerChoice >= 0.000 && computerChoice < 0.333) {
+        console.log('rock')
+    } else if (computerChoice >= 0.333 && computerChoice < 0.666) {
+        console.log('paper')
+    } else if (computerChoice >= 0.666 && computerChoice < 1) {
+        console.log('scissors')
+    } else {
+        console.log('error?')
+    }
+}
+
+getComputerChoice(choice);
